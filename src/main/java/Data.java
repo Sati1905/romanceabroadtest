c375a8eadda551b92a7dcbc57b23a089638c51c4
Satenik Sukhudyan <satenik_>
2019-09-26 21:20:07 -0700

    First steps for creating POM model
    Add 3 test classes. 1 Base - parent test class. Create Data and Locators classes.

---
+++ src/main/java/Data.java
@@ -0,0 +1,12 @@
+public class Data {
+
+    //Registration data
+    static final String email ="11@gmail.com";
+    static final String password ="338377377m";
+    static final String nickname ="paul444";
+    static final String phone ="123456789";
+
+
+    static final String expectedUrlMedia = "https://romanceabroad.com/media/index";
+    static final String expectedUrlSearch = "https://romanceabroad.com/users/search";
+}
